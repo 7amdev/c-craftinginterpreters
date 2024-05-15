3d6eee4eb421a1f6ee6703d4ce23c77169d55abf
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-05-15 04:21:53 -0100

    add arithmetic  operations

--- src/vm.c
+++ src/vm.c
@@ -6,8 +6,14 @@
 
 VM vm;
 
+void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
+    resetStack();
 }
 
 void freeVM()
@@ -18,24 +24,59 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
 
     for (;;)
     {
+
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
+
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
         case OP_CONSTANT:
         {
             Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
+            push(constant);
+            break;
+        }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+        {
+            push(-pop());
             break;
         }
         case OP_RETURN:
         {
+            printValue(pop());
+            printf("\n");
             return INTERPRET_OK;
         }
         }
@@ -43,6 +84,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
@@ -51,4 +93,16 @@ InterpretResult interpret(Chunk *chunk)
     vm.ip = vm.chunk->code;
 
     return run();
+}
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop += 1;
+}
+
+Value pop()
+{
+    vm.stackTop -= 1;
+    return *vm.stackTop;
 }
