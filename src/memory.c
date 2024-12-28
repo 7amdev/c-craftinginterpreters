7f6dc3ec95e70ec120666fdc60e72eb363b7119b
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-12-28 09:43:02 -0100

    add classes and instances

--- src/memory.c
+++ src/memory.c
@@ -112,6 +112,13 @@ static void blackendObject(Obj *object)
         markArray(&function->chunk.constants);
         break;
     }
+    case OBJ_INSTANCE:
+    {
+        ObjInstance *instance = (ObjInstance *)object;
+        markObject((Obj *)instance->klass);
+        markTable(&instance->fields);
+        break;
+    }
     case OBJ_UPVALUE:
     {
         markValue(((ObjUpvalue *)object)->closed);
@@ -132,6 +139,12 @@ static void freeObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_CLASS:
+    {
+        ObjClass *klass = (ObjClass *)object;
+        markObject((Obj *)klass->name);
+        break;
+    }
     case OBJ_CLOSURE:
     {
         ObjClosure *closure = (ObjClosure *)object;
@@ -144,6 +157,13 @@ static void freeObject(Obj *object)
         ObjFunction *function = (ObjFunction *)object;
         freeChunk(&function->chunk);
         FREE(ObjFunction, object);
+        break;
+    }
+    case OBJ_INSTANCE:
+    {
+        ObjInstance *instance = (ObjInstance *)object;
+        freeTable(&instance->fields);
+        FREE(ObjInstance, object);
         break;
     }
     case OBJ_NATIVE:
